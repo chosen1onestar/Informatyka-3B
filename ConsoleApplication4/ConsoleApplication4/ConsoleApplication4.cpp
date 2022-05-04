bb7ef21265dd2f2792ee65723ba3f74a15cdaf1d
chosen1onestar <74967017+chosen1onestar@users.noreply.github.com>
2022-05-04 20:04:08 +0200

    Fibonacci

---
+++ ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+void fibonacci(int n)
+{
+		int a = 0, b = 1;
+
+		for (int i = 0; i < n; i++)
+		{
+			cout << b << " ";
+			b += a;
+			a = b - a;
+
+
+		}
+}
+int main()
+{
+		int n;
+		cout << "Ile wyrazow ciagu wypisac : ";
+		cin >> n;
+		fibonacci(n);
+
+}
